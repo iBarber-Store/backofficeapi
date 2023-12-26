3283eb279b316b1b0c7b861eb605974ad9d76c45
edgarnd <edgar.rda08@gmail.com>
2023-12-25 21:44:29 -0600

    Finish production

--- app/config/database.py
+++ app/config/database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# connection_string = "mysql+mysqlconnector://user1:pscale_pw_abc123@us-east.connect.psdb.cloud:3306/sqlalchemy"
 connection_string = "mysql+mysqlconnector://root:root@localhost:8889/ibarber"
 engine = create_engine(connection_string, echo=True)
 
